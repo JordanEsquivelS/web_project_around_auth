2cf92e078b8f6e60a0ebf1c57f44ea7c2ea45daa
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-09-25 10:43:23 -0500

    Agregar iniciar sesion o registrate header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+  // Estado para determinar si el popup de inicio de sesión está abierto o no
+  const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false);
+
+  // Función para manejar el click en el span
+  const handleLoginClick = () => {
+    // Abrir o cerrar el popup. Aquí también puedes agregar la lógica para mostrar el popup.
+    setIsLoginPopupOpen(!isLoginPopupOpen);
+  };
   return (
     <header className="header">
       <div className="header header__logo">
@@ -9,7 +17,11 @@ function Header() {
           src={require("../images/logo.svg").default}
           alt="Logo Around"
         />
+        <button className="header__login" onClick={handleLoginClick}>
+        {isLoginPopupOpen ? "Regístrate" : "Iniciar sesión"}
+      </button>
       </div>
+
       <div className="header__line"></div>
     </header>
   );
