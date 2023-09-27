20a35efbad0d5537d0f42c3bba88750f6fa6bcd3
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-09-27 12:25:04 -0500

    Agregar auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,49 @@
+const BASE_URL = "https://register.nomoreparties.co";
+
+export const register = (password, email) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ password, email }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.statusText}`);
+    });
+};
+
+export const authorize = (password, email) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ password, email }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.statusText}`);
+    });
+};
+
+export const checkToken = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.statusText}`);
+    });
+};
